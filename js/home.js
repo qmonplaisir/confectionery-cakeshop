c1abbc517d6034b6ff841d423ca06c71ae009508
qmonplaisir <qmonplaisir@gmail.com>
2022-11-18 09:32:55 -0500

    Fixed accordion box.

--- js/home.js
+++ js/home.js
@@ -19,25 +19,6 @@ function openNav() {
   
   /*FAQ accordion*/
 
-  var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -48,7 +29,7 @@ for (i = 0; i < acc.length; i++) {
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "100px";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
